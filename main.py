2e0ae7bb237df89ed89efa1a398af582573a9228
Ankit Sachdeva <38436931+ankitsxchdeva@users.noreply.github.com>
2021-06-11 06:21:38 +0000

    Setup

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 import discord
+import os
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+  print('Bot has logged in as {0.user}'
+  .format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+  if message.content.startswith('!strava'):
+    await message.channel.send('Hello!')
+
+
+client.run(os.environ['TOKEN'])
 
